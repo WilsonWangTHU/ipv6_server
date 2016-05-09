23b74f71b8c03b2416a454fbeae4c837a7c85612
WilsonWangTHU <wode406@hotmail.com>
2016-05-09 12:59:31 +0800

    now runnable on apache2, pi

--- priClient/priClient/settings.py
+++ priClient/priClient/settings.py
@@ -41,12 +41,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
--- priClient/priClient/wsgi.py
+++ priClient/priClient/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+import django
 
-from django.core.wsgi import get_wsgi_application
+apache_conf = os.path.abspath(__file__)
+project = os.path.dirname(apache_conf)
+workspace = os.path.dirname(project)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "priClient.settings")
+sys.path.append(workspace)
+sys.path.append('/priClient/priClient')
+sys.path.append('/sbin/')
 
-application = get_wsgi_application()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['PYTHON_EGG_CACHE'] = '/tmp'
+
+django.setup()
+
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
